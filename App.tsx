e58c9bce1aca1f83f05906f8024608de3d6063b0
sirageDb <ilovesemicolons@hotmail.com>
2022-05-10 14:35:32 +0200

    CHORE : configuration

--- App.tsx
+++ App.tsx
@@ -1,22 +1,182 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import { AppRegistry } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, ApolloLink, concat } from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
+import FlashCard from "./src/screens/Flashcard";
+import ListFlashCards from "./src/screens/ListFlashCards";
+import { screenNames } from "./src/utils/screenNames";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { NavigationContainer, ParamListBase } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthContext } from "./src/utils/AuthContext";
+import { ClassroomContext } from "./src/utils/ClassroomContext";
+import * as SecureStore from 'expo-secure-store';
+import AuthScreen from "./src/screens/AuthScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator<ParamListBase>();
+const Drawer = createDrawerNavigator();
+
+//Apollo instance creatation
+//==========================================================
+const httpLink = createHttpLink({
+  uri : 'http://192.168.1.43:5000/graphql',
+})
+
+const authMidlw = new ApolloLink((operation, next) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: SecureStore.getItemAsync('userToken') || '',
+    },
+  }));
+  return next(operation);
+});
+
+const client = new ApolloClient({
+  link: concat(authMidlw, httpLink),
+  cache: new InMemoryCache(),
+});
+//==========================================================
+
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: true }}>
+      <Stack.Screen
+        name={screenNames.listFlashCards.name}
+        component={ListFlashCards}
+        options={{ title: screenNames.listFlashCards.title }}
+      />
+      <Stack.Screen
+        name={screenNames.flashCard.name}
+        component={FlashCard}
+        options={{
+          title: screenNames.flashCard.title,
+          headerStyle: {
+            backgroundColor: "#FCC300",
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AppDrawer = () => {
+  return (
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen
+        name={"listFlashCardsDrawer"}
+        component={MainStackNavigator}
+        options={{
+          title: screenNames.listFlashCards.title,
+        }}
+      />
+      <Drawer.Screen
+        name={"settingsDrawer"}
+        component={SettingsScreen}
+        options={{
+          title: screenNames.settingsScreen.title,
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+//=========================================
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const [classroomId, setClassroomId] = useState<any>("");
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  const [state, dispatch] = React.useReducer(
+    (prevState: any, action: any) => {
+      switch (action.type) {
+        case "RESTORE_TOKEN":
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case "SIGN_IN":
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case "SIGN_OUT":
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  //============================
+  React.useEffect(() => {
+    const bootstrapAsync = async () => {
+      let userToken;
+      try {
+        userToken = await SecureStore.getItemAsync("userToken");
+      } catch (e) {
+        console.log(e);
+      }
+      dispatch({ type: "RESTORE_TOKEN", token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  //===========================
+
+
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (data: any) => {
+        dispatch({ type: "SIGN_IN", data });
+        await SecureStore.setItemAsync("userToken", data.token);
+      },
+      signOut: () => dispatch({ type: "SIGN_OUT" }),
+    }),
+    []
+  );
+
+
+
+
+  return (
+    <ApolloProvider client={client}>
+      <AuthContext.Provider value={authContext}>
+        <ClassroomContext.Provider value={{classroomId, setClassroomId}}>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              {state.userToken === null ? (
+                <Stack.Screen
+                  name={screenNames.authScreen.name}
+                  component={AuthScreen}
+                  options={{ title: screenNames.authScreen.title }}
+                />
+              ) : (
+                <Stack.Screen
+                  name={screenNames.root.name}
+                  component={AppDrawer}
+                  options={{ title: screenNames.root.title }}
+                />
+              )}
+            </Stack.Navigator>
+          </NavigationContainer>
+        </ClassroomContext.Provider>
+      </AuthContext.Provider>
+    </ApolloProvider>
+  );
 }
+AppRegistry.registerComponent("MyApplication", () => App);
---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Button, View, Image, TextInput, StyleSheet,Alert } from "react-native";
+import AppLayout from "../components/Layout";
+import { screenNames } from "../utils/screenNames";
+import { AuthContext } from "../utils/AuthContext";
+import { ClassroomContext } from "../utils/ClassroomContext";
+
+export default function SettingsScreen() {
+    const {signOut} = React.useContext(AuthContext);
+
+
+  const signoutUser = () => {
+    signOut();
+  };
+
+  return (
+    <AppLayout>
+      <View>
+        <Button onPress={signoutUser} title={"Se déconnecter"} color ={"#c95252"} />
+      </View>
+    </AppLayout>
+  );
+}
